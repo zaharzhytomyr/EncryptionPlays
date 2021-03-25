d06f1b8444997ffc1c54290e4be4d3f0e5d8272d
grass field <grassfield0@gmail.com>
2021-03-25 11:53:31 +0000

    2 key encode

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #replace spaces by "_"
+sample_str = "Еврей, еще и обновленец-таких не любил; вопрос: как он ужился там в 23.04.1965?В 78м он уехал."
 
 def array_to_str(arr) -> str:
   res = ""
@@ -76,10 +77,10 @@ def decode_no_key_permutation(text : str, rows : int = 7, cols : int = 7):
     res += read_table_by_rows(table, cols)
   return res
 
-def encode_col_key_permutation(text : str, rows : int = 7, cols : int = 7,
+def encode_one_key_permutation(text : str, rows : int = 7, cols : int = 7,
                            key : str = '0145236') -> str:
   assert(len(key) == cols)
-  key_map = {int(val): num for num, val in enumerate(key)}
+  key_map = {int(val) - 1: num for num, val in enumerate(key)}
   
   res = ""
   fragments = split_by_len(text, rows * cols)
@@ -94,11 +95,31 @@ def encode_col_key_permutation(text : str, rows : int = 7, cols : int = 7,
     res += read_table_by_rows(encoded_table, cols)
   return res
 
-def decode_col_key_permutation(text : str, rows : int = 7, cols : int = 7,
+def decode_one_key_permutation(text : str, rows : int = 7, cols : int = 7,
                            key : str = '0145236'):
   assert(len(key) == cols)
-  key_map = {int(val): num for num, val in enumerate(key)}
+  key_map = {int(val) - 1: num for num, val in enumerate(key)}
   
+  res = ""
+  fragments = split_by_len(text, rows * cols)
+  for fragment in fragments:
+    first_table = create_empty_table(rows, cols)
+    fill_table_by_rows(first_table, fragment, rows, cols)
+
+    decoded_table = create_empty_table(rows, cols)
+    for i in key_map.keys():
+      col = get_col(first_table, key_map[i])
+      set_col(decoded_table, i, col)
+    res += read_table_by_cols(decoded_table, cols)
+  return res
+
+def encode_two_keys_permutation(text : str, rows : int = 7, cols : int = 7,
+                                key_cols : str = '0145236',
+                                key_rows : str = '0362154'):
+  assert(len(key_rows) == rows)
+  key_map = {int(val) - 1: num for num, val in enumerate(key_rows)}
+  text = encode_one_key_permutation(text, rows, cols, key_cols)
+
   res = ""
   fragments = split_by_len(text, rows * cols)
   for fragment in fragments:
@@ -106,21 +127,26 @@ def decode_col_key_permutation(text : str, rows : int = 7, cols : int = 7,
     fill_table_by_rows(first_table, fragment, rows, cols)
     print(first_table)
 
-    decoded_table = create_empty_table(rows, cols)
+    encoded_table = create_empty_table(rows, cols)
     for i in key_map.keys():
-      col = get_col(first_table, key_map[i])
-      set_col(decoded_table, i, col)
-    res += read_table_by_cols(decoded_table, cols)
-    print(decoded_table)
+      row = first_table[key_map[i]]
+      encoded_table[i] = row
+    res += read_table_by_rows(encoded_table, rows)
+    print(encoded_table)
   return res
 
-def encode_row_key_permutation(text : str, rows : int = 7, cols : int = 7,
-                                  key_cols : str = '0145236',
-                                  key_rows : str = '0362154'):
+def decode_two_keys_permutation(text : str, rows : int = 7, cols : int = 7,
+                                key_cols : str = '0145236',
+                                key_rows : str = '0362154'):
   return NotImplemented
+  #par 1 decode rows
+  #par2 - call decode 1 key
 
-def decode_row_key_permutation():
-  return NotImplemented
+a = encode_one_key_permutation('abcinfopapkadoma', 4, 4, '3214')
+print(decode_one_key_permutation(a, 4, 4, '3214'))
+print(encode_two_keys_permutation('abcinfopapkadoma', 4, 4, '3214', '1342'))
+
+
 
 
 
